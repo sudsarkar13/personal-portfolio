f9e1679489f903ebc50f0098515a256def8ab4c8
SUDEEPTA SARKAR <67630579+sudsarkar13@users.noreply.github.com>
2023-07-05 18:21:26 +0530

    u

---
+++ js/script.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+
+    // Marquee effect
+    $("#marq").css("background-color", "yellow");
+    $("#marq").css("font-weight", "bold");
+
+    // Body
+    $("body").css("background-color", "white");
+
+    // Navigation
+    // $("#navbar").css();
+    $("#navbar").css("background-color", "white-smoke");
+    $("#navbar").css("position", "static");
+    $("#navbar").css("top", "0");
+    $("#navbar").css("width", "80%");
+    $("#navbar").css("align-items", "center");
+    $("#navbar").css("margin", "auto");
+    $("#navbar").css("padding", "2px");
+    $("#navbar").css("border-radius", "10px");
+    $("#navbar").css("display", "flex");
+    $("#navbar").css("justify-content", "space-between");
+    $("#navbar").css("font-weight", "bold");
+    $("#navlogo").css("height", "50px");
+    $("#navlogo").css("width", "80px");
+    $("#navlogo").css("margin-top", "4px");
+    $("#navlogo").css("margin-left", "100%");
+    $("#navlogo").css("cursor", "pointer");
+    $("#navlogo").css("position", "static");
+    $("#navlogo").css("");
+    $("#navlogo").css();
+});    
