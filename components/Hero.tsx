0d2df6c777fd97d82a57af0821c69c4e0d7d1094
SUDEEPTA SARKAR <67630579+sudsarkar13@users.noreply.github.com>
2024-02-12 17:08:17 +0530

    update

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -17,7 +17,7 @@ const Hero = () => {
 						HI, I'M <span className='text-yellow-400'>SUDEEPTA!</span>
 					</h1>
 					<TextEffect />
-					<p className='mt-[2rem] text-[20px] text-[#ffffff92]'>
+					<p className='mt-[2rem] text-[18px] text-[#ffffff92]'>
 						A highly motivated B.Tech graduate with a passion for Web
 						Development, User Experience, and Product Management. Possesses 5
 						years of freelancing experience, demonstrating strong team spirit,
@@ -26,9 +26,9 @@ const Hero = () => {
 						and innovative IT organization.
 					</p>
 					<div className='mt-[2rem] flex-col space-y-6 sm:space-y-0 sm:flex sm:flex-row items-center sm:space-x-6'>
-						<button className='px-[2rem] bg-yellow-400 hover:bg-[#55e6a5] transition-all duration-200 py-[1rem] text-[18px] font-bold uppercase text-black flex items-center space-x-2'>
-							<ArrowDownTrayIcon className='w-[1.6rem]' />
+						<button className='px-[2rem] bg-yellow-400 hover:bg-[#55e6a5] transition-all duration-200 py-[1rem] text-[18px] font-bold uppercase text-black flex items-center space-x-2 rounded-md'>
 							<p>Download Resume</p>
+							<ArrowDownTrayIcon className='w-[1.6rem] h-[1.7rem] text-black' />
 						</button>
 					</div>
 				</div>
