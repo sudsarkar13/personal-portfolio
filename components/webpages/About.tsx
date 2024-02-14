a6b20b9d9ec72163a71e02f3c6ae7629cbca9e2d
SUDEEPTA SARKAR <67630579+sudsarkar13@users.noreply.github.com>
2024-02-14 20:25:40 +0530

    Update About.tsx

--- components/webpages/About.tsx
+++ components/webpages/About.tsx
@@ -8,9 +8,9 @@ const About = () => {
 		<div className='bg-[#0f1722] pb-[3rem] pt-[4rem] md:pt-[8rem]'>
 			<div className='grid grid-cols-1 md:grid-cols-2 w-[80%] mx-auto gap-[3rem] items-center'>
 				<div>
-					<h1 className='text-[20px] font-bold uppercase text-yellow-400 mb-[1rem]'>
+					{/* <h1 className='text-[20px] font-bold uppercase text-yellow-400 mb-[1rem]'>
 						ABOUT ME
-					</h1>
+					</h1> */}
 					<h2 className='text-[25px] md:text-[35px] lg:text-[45px] md:leading[3rem] leading-[2rem] capitalize mb-[3rem] font-bold text-white'>
 						Shaping <span className='text-yellow-400'>Ideas</span>
 					</h2>
@@ -22,21 +22,26 @@ const About = () => {
 								sun.
 							</p>
 							<p className='text-[19px] text-slate-300 w-[80%]'>
-								This quote encourages personal growth and resilience. It
-								emphasizes thriving in your current circumstances while aiming
-								for aspirations. It's concise, memorable, and applicable to
-								various situations.
+								We're here to help you achieve your goals.
 							</p>
 						</div>
 					</div>
-					<button className='px-[2rem] bg-yellow-400 hover:bg-[#55e6a5] transition-all duration-200 py-[1rem] text-[18px] font-bold uppercase text-black flex items-center space-x-2 rounded-md'>
-						<p>Download Resume</p>
-						<ArrowDownTrayIcon className='w-[1.6rem] h-[1.7rem] text-black' />
-					</button>
+					<a href='/files/RSCV.pdf' target='_blank'>
+						<button className='px-[2rem] bg-yellow-400 hover:bg-[#55e6a5] transition-all duration-200 py-[1rem] text-[18px] font-bold uppercase text-black flex items-center space-x-2 rounded-md'>
+							<p>Download Resume</p>
+							<ArrowDownTrayIcon className='w-[1.6rem] h-[1.7rem] text-black' />
+						</button>
+					</a>
 				</div>
-			</div>
-			<div>
-				<Image src='/images/about.jpg' alt='about' layout='fill' />
+				<div className='lg:w-[500px] mx-auto md:mx-0 mt-[2rem] lg:mt-0 lg:h-[500px] w-[300px] h-[300px] relative'>
+					<Image
+						src='/images/about.jpg'
+						alt='user'
+						layout='fill'
+						objectFit='contain'
+						className='relative z-[11] w-[100%] h-[100%] object-contain  '
+					/>
+				</div>
 			</div>
 		</div>
 	);
