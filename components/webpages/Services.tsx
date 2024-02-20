3ecc8b235a6ba2a17603cdf25a41f771f76b8005
SUDEEPTA SARKAR <67630579+sudsarkar13@users.noreply.github.com>
2024-02-20 18:54:29 +0530

    Update Services.tsx

--- components/webpages/Services.tsx
+++ components/webpages/Services.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 const Services = () => {
-	return <div>Services</div>;
+	return <div className='bg-[#0c121c] pt-[4rem] md:pt-[8rem] pb-[5rem]'>
+		<p className="text-center text-white text-[28px] sm:text-[33px] md:text-[45px] font-bold uppercase ">My <span className="text-yellow-400">Services</span> </p>
+	</div>;
 };
 
 export default Services;
