3bc28765b349c343907120aa4aec3181e8761b4d
SUDEEPTA SARKAR <67630579+sudsarkar13@users.noreply.github.com>
2024-02-21 23:24:23 +0530

    Minimal changes

--- components/webpages/Services.tsx
+++ components/webpages/Services.tsx
@@ -3,9 +3,14 @@ import React from "react";
 const Services = () => {
 	return (
 		<div className='bg-[#0c121c] pt-[4rem] md:pt-[8rem] pb-[5rem]'>
-			<p className='text-center text-white text-[28px] sm:text-[33px] md:text-[45px] font-bold uppercase '>
+			<p className='heading'>
 				My <span className='text-yellow-400'>Services</span>
 			</p>
+			<div className='services-area'>
+				<div>1ST</div>
+				<div>2ND</div>
+				<div>3RD</div>
+			</div>
 		</div>
 	);
 };
