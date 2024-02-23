b11f643db219c1c19a0cad89ae6b1ed02c203565
SUDEEPTA SARKAR <67630579+sudsarkar13@users.noreply.github.com>
2024-02-23 07:24:43 +0530

    Optimising Update

--- components/webpages/Skills.tsx
+++ components/webpages/Skills.tsx
@@ -1,7 +1,35 @@
 import React from "react";
+import SkillsItem from "./CodeOptimiser/skills/SkillsItem";
+import SkillsLanguage from "./CodeOptimiser/skills/SkillsLanguage";
 
 const Skills = () => {
-	return <div>Skills</div>;
+	return (
+		<div className='pt-[4rem] md:pt-[8rem] pb-[5rem] bg-[#09101a] '>
+			<h1 className='heading'>
+				Education & <span className='text-yellow-400 '>Skills</span>
+			</h1>
+			<div className='w-[80%] mx-auto pt-[4rem] md:pt-[8rem] grid grid-cols-1 md:grid-cols-2 gap-[2rem] items-center'>
+				<div>
+					<SkillsItem title='React Developer' year='2023' />
+					<SkillsItem title='React Developer' year='2023' />
+					{/* <SkillsLanguage skill-1="HTML" skill-2="CSS" skill-3="Javascript" skill-4="Typescript" skill-5="React" skill-6="NextJS" skill-7="Amazon Web Services" skill-8="UI/UX" /> */}
+					<SkillsLanguage
+						skill1='HTML'
+						skill2='CSS'
+						skill3='Javascript'
+						level1='80%'
+						level2='80%'
+						level3='80%'
+					/>
+				</div>
+				<div>
+					<SkillsItem />
+					<SkillsItem />
+					<SkillsLanguage />
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Skills;
