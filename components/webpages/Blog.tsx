c92d8dc7241f479a72a8639276e943e449ecc77f
SUDEEPTA SARKAR <67630579+sudsarkar13@users.noreply.github.com>
2024-02-26 20:39:40 +0530

    Update Blog.tsx

--- components/webpages/Blog.tsx
+++ components/webpages/Blog.tsx
@@ -1,7 +1,29 @@
 import React from "react";
+import Image from "next/legacy/image";
 
 const Blog = () => {
-	return <div>Blog</div>;
+	return (
+		<div className='pt-[4rem] md:pt-[8rem] pb-[4rem] bg-[#02050a]'>
+			<h1 className='heading'>
+				My
+				<span className='text-yellow-400'> Blog</span>
+			</h1>
+			<div className='grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 mt-[4rem] gap-[3rem] w-[80%] mx-auto'>
+				<div>
+					<div className='w-[100%] relative h-[400px] '>
+						<Image
+							src='/images/blog1.jpg'
+							alt='blog'
+							layout='fill'
+							className='object-cover'
+						/>
+					</div>
+				</div>
+				<div></div>
+				<div></div>
+			</div>
+		</div>
+	);
 };
 
 export default Blog;
