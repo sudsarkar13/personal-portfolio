a7a8f886c6e5cc44c58efcb9b610b04cf9948944
SUDEEPTA SARKAR <67630579+sudsarkar13@users.noreply.github.com>
2024-02-27 22:42:51 +0530

    Update GitHubContribution.tsx

--- components/githubContributions/GitHubContribution.tsx
+++ components/githubContributions/GitHubContribution.tsx
@@ -2,11 +2,17 @@ import React from "react";
 import GitHubCalendar from "react-github-calendar";
 
 const GitHubContribution = () => {
-    const currYear = new Date().getFullYear();
+	const currYear = new Date().getFullYear();
 	return (
 		<div>
-            <h1 className="heading-small">GitHub <span className="text-yellow-400">Contributions</span></h1>
-			<GitHubCalendar username={"sudsarkar13"} year={currYear} colorScheme={"dark"} />
+			<h1 className='heading-small'>
+				GitHub <span className='text-yellow-400'>Contributions</span>
+			</h1>
+			<GitHubCalendar
+				username={"sudsarkar13"}
+				year={currYear}
+				colorScheme={"dark"}
+			/>
 			{/* <GitHubCalendar username={"sudsarkar13"} colorScheme={"dark"} /> */}
 		</div>
 	);
