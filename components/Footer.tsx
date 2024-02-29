63d8bdd4d5992a5627f8be46c77618c96f233697
SUDEEPTA SARKAR <67630579+sudsarkar13@users.noreply.github.com>
2024-02-29 15:33:07 +0530

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -11,7 +11,7 @@ const Footer = () => {
 	return (
 		<div className='footer'>
 			<div className='footer-1'>
-				<div className='footer-2'>
+				{/* <div className='footer-2'>
 					<div className='footer-3'>
 						<MapIcon className='footer-icon' />
 					</div>
@@ -19,42 +19,40 @@ const Footer = () => {
 						<h1 className='footer-address'>Address</h1>
 						<p className='footer-address-p'>Lorem ipsum dolor sit amet.</p>
 					</div>
-				</div>
-				<div className='footer-2'>
+				</div> */}
+				{/* <div className='footer-2'>
 					<div className='footer-3'>
 						<DevicePhoneMobileIcon className='footer-icon' />
 					</div>
 					<div>
 						<h1 className='footer-address'>Phone</h1>
-						{/* <p className='footer-address-p'>+91<span className="p-[2px]"></span>7504614781</p> */}
 						<p className='footer-address-p'>
 							+91<span className='p-[2px]'></span>75XXXXXXX1 <br />
 							+91<span className='p-[2px]'></span>78XXXXXXX5
 						</p>
 					</div>
-				</div>
-				<div className='footer-2'>
+				</div> */}
+				{/* <div className='footer-2'>
 					<div className='footer-3'>
 						<EnvelopeIcon className='footer-icon' />
 					</div>
 					<div>
 						<h1 className='footer-address'>Email me @</h1>
-						{/* <p className='footer-address-p'>+91<span className="p-[2px]"></span>7504614781</p> */}
 						<p className='footer-address-p'>
 							example.co.in <br />
 							example@example.com
 						</p>
 					</div>
-				</div>
+				</div> */}
 			</div>
 			<div className='footer-4'>
 				<div className='footer-txt'>
 					<p>Copyright © {curryear}, Sudeepta Sarkar, All rights reserved.</p>
 				</div>
-				<div className="footer-5">
-					<p className="footer-txt-p">Terms & Conditions</p>
-					<p className="footer-txt-p">Privacy Policy</p>
-					<p className="footer-txt-p">Sitemap</p>
+				<div className='footer-5'>
+					<p className='footer-txt-p'>Terms & Conditions</p>
+					<p className='footer-txt-p'>Privacy Policy</p>
+					<p className='footer-txt-p'>Sitemap</p>
 				</div>
 			</div>
 		</div>
