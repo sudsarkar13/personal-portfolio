eb0d5e51fd4b7d71032022b5943cb9d05c50c7af
SUDEEPTA SARKAR <67630579+sudsarkar13@users.noreply.github.com>
2024-02-29 12:11:32 +0530

    Update About.tsx

--- components/webpages/About.tsx
+++ components/webpages/About.tsx
@@ -8,7 +8,7 @@ import Quote from "../extras/quote/Quote";
 
 const About = () => {
 	return (
-		<div className='bg-[#0f1722] pb-[3rem] pt-[4rem] md:pt-[8rem]'>
+		<div className='bg-[#0f1722] pb-[3rem] pt-[2rem] md:pt-[6rem]'>
 			<div className='grid grid-cols-1 md:grid-cols-2 w-[80%] mx-auto gap-[3rem] items-center'>
 				<div>
 					<h1 className='text-[20px] font-bold uppercase text-yellow-400 mb-[1rem]'>
