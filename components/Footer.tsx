40dcde78f5b29b57a807ba6e9359ad4949fff50e
SUDEEPTA SARKAR <67630579+sudsarkar13@users.noreply.github.com>
2024-02-29 23:55:01 +0530

    Deployment Ready

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -11,45 +11,48 @@ const Footer = () => {
 	return (
 		<div className='footer'>
 			<div className='footer-1'>
-				{/* <div className='footer-2'>
+				<div className='footer-2'>
 					<div className='footer-3'>
 						<MapIcon className='footer-icon' />
 					</div>
 					<div>
 						<h1 className='footer-address'>Address</h1>
-						<p className='footer-address-p'>Lorem ipsum dolor sit amet.</p>
+						<p className='footer-address-p'>
+							P.C. Sarkar Lane, <br /> Arunodaya Nagar, Cuttack, Odisha, Pincode
+							- 753012 <br /> PS.: Badambadi
+						</p>
 					</div>
-				</div> */}
-				{/* <div className='footer-2'>
+				</div>
+				<div className='footer-2'>
 					<div className='footer-3'>
 						<DevicePhoneMobileIcon className='footer-icon' />
 					</div>
 					<div>
 						<h1 className='footer-address'>Phone</h1>
 						<p className='footer-address-p'>
-							+91<span className='p-[2px]'></span>75XXXXXXX1 <br />
-							+91<span className='p-[2px]'></span>78XXXXXXX5
+							+91<span className='p-[2px]'></span>7504614781 <br />
+							+91<span className='p-[2px]'></span>7809518625
 						</p>
 					</div>
-				</div> */}
-				{/* <div className='footer-2'>
+				</div>
+				<div className='footer-2'>
 					<div className='footer-3'>
 						<EnvelopeIcon className='footer-icon' />
 					</div>
 					<div>
 						<h1 className='footer-address'>Email me @</h1>
 						<p className='footer-address-p'>
-							example.co.in <br />
-							example@example.com
+							sudeeptasarkarbiz@gmail.com <br />
+							sudsarkar13@gmail.com
 						</p>
 					</div>
-				</div> */}
+				</div>
 			</div>
 			<div className='footer-4'>
 				<div className='footer-txt'>
 					<p>Copyright © {curryear}, Sudeepta Sarkar, All rights reserved.</p>
 				</div>
-				<div className='footer-5'>
+				<div className='footer-5 hidden'>
 					<p className='footer-txt-p'>Terms & Conditions</p>
 					<p className='footer-txt-p'>Privacy Policy</p>
 					<p className='footer-txt-p'>Sitemap</p>
