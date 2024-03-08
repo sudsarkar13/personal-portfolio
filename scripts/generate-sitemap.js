b42e7b09a02ea3dd651c2316a28842533d07c35b
SUDEEPTA SARKAR <67630579+sudsarkar13@users.noreply.github.com>
2024-03-09 02:02:11 +0530

    09/03/2024  Final Update

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 
 sitemap({
 	baseUrl: "https://portfolio-sudeeptasarkar.vercel.app",
-	pagesDirectory: path.join(__dirname, "../components"),
+	pagesDirectory: path.join(__dirname, "../components/webpages"),
 	targetDirectory: path.join(__dirname, "../public"),
 	ignoredExtensions: ["js", "map", "json", "xml", "png", "jpg", "jpeg", "svg"],
 	ignoredPaths: ["favicon", "index", "404", "500"],
