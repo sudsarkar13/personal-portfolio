9acd6b1990e5ebfaf669ae9f84ef3558b48c1e32
SUDEEPTA SARKAR <67630579+sudsarkar13@users.noreply.github.com>
2024-04-14 16:13:20 +0530

    Update vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,27 +4,28 @@ import Head from "next/head";
 import { useRouter } from "next/router";
 import Script from "next/script";
 import { useEffect } from "react";
+import { Analytics } from "@vercel/analytics/react";
 import * as gtag from "@/lib/gtag"; // Make sure this path is correct based on your project structure
 
 const App = ({ Component, pageProps }: AppProps) => {
-  const router = useRouter();
+	const router = useRouter();
 
-  useEffect(() => {
-    const handleRouteChange = (url: string) => {
-      gtag.pageview(url);
-    };
-    router.events.on("routeChangeComplete", handleRouteChange);
-    return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
-    };
-  }, [router.events]);
+	useEffect(() => {
+		const handleRouteChange = (url: string) => {
+			gtag.pageview(url);
+		};
+		router.events.on("routeChangeComplete", handleRouteChange);
+		return () => {
+			router.events.off("routeChangeComplete", handleRouteChange);
+		};
+	}, [router.events]);
 
-  return (
-    <>
-      <Head>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
+	return (
+		<>
+			<Head>
+				<script
+					dangerouslySetInnerHTML={{
+						__html: `
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
@@ -33,17 +34,18 @@ const App = ({ Component, pageProps }: AppProps) => {
                 page_path: window.location.pathname,
               });
             `,
-          }}
-        />
-      </Head>
-      {/* Global Site Tag (gtag.js) - Google Analytics */}
-      <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
-      />
-      <Component {...pageProps} />
-    </>
-  );
+					}}
+				/>
+			</Head>
+			{/* Global Site Tag (gtag.js) - Google Analytics */}
+			<Script
+				strategy='afterInteractive'
+				src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+			/>
+			<Analytics />
+			<Component {...pageProps} />
+		</>
+	);
 };
 
 export default App;
