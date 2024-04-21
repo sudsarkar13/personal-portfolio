8fbdce1da59c9074d6fdc5884380356a5ca7066c
SUDEEPTA SARKAR <67630579+sudsarkar13@users.noreply.github.com>
2024-04-22 01:39:39 +0530

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { GoogleTagManager, GoogleAnalytics } from "@next/third-parties/google";
 import Adsense from "@/components/Google/Adsense";
+import Head from "next/head";
 
 export const GTM_ID: string = process.env.NEXT_PUBLIC_GTM_ID || "default-value";
 export const GA_ID: string = process.env.NEXT_PUBLIC_GA_ID || "default-value";
@@ -12,6 +13,9 @@ export const GAD_ID: string = process.env.NEXT_PUBLIC_GADS_ID || "default-value"
 export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<>
+		<Head>
+			<Adsense pubId={GAD_ID} />
+		</Head>
 			<Analytics />
 			<SpeedInsights />
 			<GoogleTagManager gtmId={GTM_ID} />
