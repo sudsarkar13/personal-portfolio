1073a111693fc30573d87d99c3ee3607aa097abc
SUDEEPTA SARKAR <67630579+sudsarkar13@users.noreply.github.com>
2024-04-22 02:02:39 +0530

    Fix try: Client-side

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,6 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
-import { GoogleTagManager, GoogleAnalytics } from "@next/third-parties/google";
 import Adsense from "@/components/Google/Adsense";
 import Head from "next/head";
 
@@ -19,8 +18,6 @@ export default function App({ Component, pageProps }: AppProps) {
 			</Head>
 			<Analytics />
 			<SpeedInsights />
-			<GoogleTagManager gtmId={GTM_ID} />
-			<GoogleAnalytics gaId={GA_ID} />
 			<Component {...pageProps} />
 		</>
 	);
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -14,9 +14,9 @@ export default function Document() {
 			</Head>
 			<body>
 				<Main />
+				<NextScript />
 				<GoogleTagManager gtmId={GTM_ID} />
 				<GoogleAnalytics gaId={GA_ID} />
-				<NextScript />
 			</body>
 		</Html>
 	);
