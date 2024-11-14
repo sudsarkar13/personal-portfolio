99ca55173caee61a304321954da8d6f1b20bab7d
SUDEEPTA SARKAR <sudsarkar13@gmail.com>
2024-11-14 20:08:42 +0530

    feat ✨: Enhance Ad component tagging logic with new meta tag
    - Added a new/meta tag and enhanced Ad component tagging logic

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 import { GoogleTagManager, GoogleAnalytics } from "@next/third-parties/google";
 import Adsense from "@/components/Google/Adsense";
 import Script from "next/script";
-import AdBanner from '../components/Google/AdBanner';
+import AdBanner from "../components/Google/AdBanner";
 
 export const GTM_ID: string = process.env.NEXT_PUBLIC_GTM_ID || "default-value";
 export const GA_ID: string = process.env.NEXT_PUBLIC_GA_ID || "default-value";
@@ -11,19 +11,20 @@ export const GAD_ID: string =
 
 export default function Document() {
 	return (
-		<Html lang='en'>
+		<Html lang="en">
 			<Head>
 				<Adsense pubId={GAD_ID} />
+				<meta
+					name="google-adsense-account"
+					content="ca-pub-9301461712290016"></meta>
 				<script
 					async
-					custom-element='amp-ad'
-					src='https://cdn.ampproject.org/v0/amp-ad-0.1.js'
-				></script>
+					custom-element="amp-ad"
+					src="https://cdn.ampproject.org/v0/amp-ad-0.1.js"></script>
 				<Script
 					async
-					src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9301461712290016'
-					crossOrigin='anonymous'
-				></Script>
+					src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9301461712290016"
+					crossOrigin="anonymous"></Script>
 			</Head>
 			<body>
 				<Main />
@@ -33,14 +34,13 @@ export default function Document() {
 				{/* <AdBanner dataAdSlot="1" dataAdFormat="auto" dataFullWidthResponsive={true} /> */}
 				<AdBanner adCode={GAD_ID} />
 				<amp-ad
-					width='100vw'
-					height='320'
-					type='adsense'
-					data-ad-client='ca-pub-9301461712290016'
-					data-ad-slot='3782882970'
-					data-auto-format='rspv'
-					data-full-width=''
-				>
+					width="100vw"
+					height="320"
+					type="adsense"
+					data-ad-client="ca-pub-9301461712290016"
+					data-ad-slot="3782882970"
+					data-auto-format="rspv"
+					data-full-width="">
 					<div style={{ overflow: "auto" }}></div>
 				</amp-ad>
 			</body>
